d4b522da80881ee9fffc99536ad9ef1869f23a26
Lulzx <wiserishabh@gmail.com>
2019-06-16 22:08:09 +0530

    rearranged some stuff

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from bs4 import Comment
 from newspaper import Article
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegraph import Telegraph
+from lxml.html import fromstring
 from lxml.html.clean import clean_html, unicode
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
@@ -27,11 +28,6 @@ def start(bot, update):
 def find(string):
     url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
     return url
-
-
-VALID_TAGS = dict(a=['href', 'title'], aside=[], b=[], blockquote=[], br=[], code=[], em=[], figcaption=[], figure=[],
-                  h3=[], h4=[], hr=[], i=[], iframe=[], img=['src'], li=[], ol=[], p=[], pre=[], s=[], strong=[], u=[],
-                  ul=[], video=[])
 
 
 def to_unicode(s):
@@ -48,34 +44,6 @@ def to_unicode(s):
     return unicode(''.join([i if ord(i) < 128 else ' ' for i in s]))
 
 
-def sanitize_html(value, valid_tags=None):
-    if valid_tags is None:
-        valid_tags = VALID_TAGS
-    response = requests.get(f'{value}')
-    value = response.content
-    value = clean_html(value)
-#    value = re.sub(r'[^\x00-\x7F]+', ' ', value)
-#    value = value.replace("\r", "").replace("\t", "")
-    soup = BeautifulSoup(value, 'html.parser')
-    for comments in soup.findAll(text=lambda text: isinstance(text, Comment)):
-        comments.extract()
-    [s.extract() for s in soup('style')]
-    new = soup.renderContents()
-    while 1:
-        old = new
-        soup = BeautifulSoup(new, 'html.parser')
-        for tag in soup.findAll(True):
-            if tag.name not in valid_tags:
-                tag.hidden = True
-            else:
-                tag.attrs = {attr: value for attr, value in tag.attrs.items() if attr in valid_tags[tag.name]}
-        new = soup.renderContents()
-        if old == new:
-            break
-    new = new.decode()
-    return new
-
-
 def help(bot, update):
     update.message.reply_text('Help!')
 
@@ -87,7 +55,11 @@ def echo(bot, update):
     article = Article(url)
     article.download()
     article.parse()
-    title = article.title
+    VALID_TAGS = dict(a=['href', 'title'], aside=[], b=[], blockquote=[], br=[], code=[], em=[], figcaption=[],
+                      figure=[],
+                      h3=[], h4=[], hr=[], i=[], iframe=[], img=['src'], li=[], ol=[], p=[], pre=[], s=[], strong=[],
+                      u=[],
+                      ul=[], video=[])
     try:
         author = "✍ *Author:* " + article.authors + "\n"
     except:
@@ -96,13 +68,11 @@ def echo(bot, update):
     try:
         date += str(article.publish_date.strftime('%Y-%m-%d'))
     except:
-        if article.publish_date is not None:
-            date += str(article.publish_date)
-        else:
+        if article.publish_date is None:
             date = ""
+        else:
+            date += str(article.publish_date)
     text = article.text
-    update_id = update.update_id
-    chat_id = update.message.chat_id
     article.nlp()
     keywords = article.keywords
     tags = ""
@@ -110,23 +80,47 @@ def echo(bot, update):
         tags += " #" + keyword
     summary = article.summary
     read = readtime.of_text(text)
-    text = f"""🔗 *Link:* [ {url} ]\n{author}{date}\n🚩 *Title: {title}*\n🗨 *Summarize:* _{summary}_\n"""
-    text += f"""\n🤔 *Reading Time:* {read}\n📑 *Tags:* {tags}\n """
-    update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN)
     html = article.html
-    links = find(html)
-    filepath = r"C:/Users/Lulzx/Documents/sumitup-master/temp/{}.txt".format(update_id)
-    with open(filepath, "w+") as file:
-        for link in links:
-            file.write("{}\n".format(link))
-        file.close()
-    bot.send_document(chat_id=chat_id, document=open(filepath, 'rb'))
-    # image = article.top_image
-    telegraph = Telegraph()
-    html = sanitize_html(url)
+    # links = find(html)
+    # update_id = update.update_id
+    # chat_id = update.message.chat_id
+    # filepath = r"C:/Users/Lulzx/Documents/sumitup-master/temp/{}.txt".format(update_id)
+    # with open(filepath, "w+") as file:
+    #    for link in links:
+    #        file.write("{}\n".format(link))
+    #    file.close()
+    # bot.send_document(chat_id=chat_id, document=open(filepath, 'rb'))
+    response = requests.get(url)
+    value = response.content
+    tree = fromstring(value)
+    title = str(tree.findtext('.//title'))
+    value = clean_html(value)
+    #    value = re.sub(r'[^\x00-\x7F]+', ' ', value)
+    #    value = value.replace("\r", "").replace("\t", "")
+    soup = BeautifulSoup(value, 'html.parser')
+    for comments in soup.findAll(text=lambda text: isinstance(text, Comment)):
+        comments.extract()
+    [s.extract() for s in soup('style')]
+    msg = f"""🔗 *Link:* [ {url} ]\n{author}{date}\n🚩 *Title: {title}*\n🗨 *Summarize:* _{summary}_\n"""
+    msg += f"""\n🤔 *Reading Time:* {read}\n📑 *Tags:* {tags}\n """
+    update.message.reply_text(msg, parse_mode=telegram.ParseMode.MARKDOWN)
+    new = soup.renderContents()
+    while 1:
+        old = new
+        soup = BeautifulSoup(new, 'html.parser')
+        for tag in soup.findAll(True):
+            if tag.name not in VALID_TAGS:
+                tag.hidden = True
+            else:
+                tag.attrs = {attr: value for attr, value in tag.attrs.items() if attr in VALID_TAGS[tag.name]}
+        new = soup.renderContents()
+        if old == new:
+            break
+    html = new.decode()
     user = update.message.chat.first_name
+    telegraph = Telegraph()
     telegraph.create_account(short_name=user, author_name=author)
-    response = telegraph.create_page('{}'.format(title), author_name=author, html_content="{}".format(html))
+    response = telegraph.create_page(f'{title}', author_name=f'{author}', html_content=f"{html}")
     link = 'https://telegra.ph/{}'.format(response['path'])
     update.message.reply_text(link)
 
