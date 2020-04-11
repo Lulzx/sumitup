627f20381145bb63fca141bd9fdbd272904ebbf3
Lulzx <wiserishabh@gmail.com>
2020-04-11 12:38:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def main():
     dp.add_error_handler(error)
     updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
-                      url_path=TOKEN)
+                      url_path=token)
     updater.bot.set_webhook("https://sumitup.herokuapp.com/" + token)
     logger.info("Ready to rock..!")
     updater.idle()
