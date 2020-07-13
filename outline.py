b2fee46358cac6c583ac2cd04281d1fe3a80fb29
lulzx <wiserishabh@gmail.com>
2020-07-13 22:50:32 +0530

    outline experiment

--- outline.py
+++ outline.py
@@ -1,8 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
-url = 'https://www.smashingmagazine.com/2020/03/introduction-alpinejs-javascript-framework/'
+from bs4 import BeautifulSoup as bs
+url = 'https://motherfuckingwebsite.com'
 reqs = requests.get(url)
 soup = bs(reqs.text, 'lxml')
+headings = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
+tag = lambda x: str(x)[:4]
+l = list(map(tag, headings))
+d = {}
+for i in l:
+    if i in d:
+        d[i] += 1
+    else:
+        d[i] = 1
+print(d)
 print("Outline:")
-for heading in soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"]):
-    print(int(heading.name[1:])*' ' + '- ' + heading.text.strip())
+outline = ""
+for heading in headings:
+    heading_text = heading.text.strip()
+    if heading.name in ["h1", "h2"]:
+        heading_text = f"{heading_text}"
+    outline += int(heading.name[1:])*' ' + '- ' + heading_text + '\n'
+print(outline)
